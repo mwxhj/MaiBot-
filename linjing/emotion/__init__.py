9b20b6d2ae5e52c429c0b18b74723ea62cc4821f
mingwang <464272753@qq.com>
2025-04-20 04:36:52 +0800

    v0.4.3.24

--- linjing/emotion/__init__.py
+++ linjing/emotion/__init__.py
@@ -4,13 +4,14 @@
 该模块负责管理机器人的情绪状态，包括情绪变化、情绪影响因素和情绪对回复的影响。
 """
 
-from .emotion_manager import EmotionManager, EmotionState
-from .mood_model import MoodModel
+from .emotion_manager import EmotionManager # 只导入 EmotionManager
+from .mood_model import MoodModel, MoodState # 从 mood_model 导入 MoodState
 from .emotion_rules import EmotionRules
 
 __all__ = [
     'EmotionManager',
-    'EmotionState',
+    # 'EmotionState', # 移除不存在的类
+    'MoodState', # 导出实际使用的 MoodState
     'MoodModel',
     'EmotionRules'
 ] 
