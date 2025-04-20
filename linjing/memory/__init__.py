acef637de19d18a40f3df2721c1138b0110e47b7
mingwang <464272753@qq.com>
2025-04-20 13:44:39 +0800

    v0.4.6.2

--- linjing/memory/__init__.py
+++ linjing/memory/__init__.py
@@ -5,15 +5,15 @@
 记忆系统模块，负责管理机器人的长短期记忆和知识检索。
 """
 
-from linjing.memory.memory_manager import MemoryManager, Memory
+from linjing.memory.memory_manager import MemoryManager # <-- 移除 Memory
 from linjing.memory.vector_store import VectorStore
 from linjing.memory.memory_retriever import MemoryRetriever
 from linjing.constants import MemoryType
 
 __all__ = [
     'MemoryManager',
-    'Memory',
+    # 'Memory', # <-- 移除 Memory
     'VectorStore',
     'MemoryRetriever',
     'MemoryType',
-] 
+]
